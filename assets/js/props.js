89e8f4b6203037eebfd3acd7837f1c32514fc843
ThomAlmeidaRD <thomas.almeidard@gmail.com>
2021-06-18 19:20:53 -0300

    Estrutura Pronta

---
+++ assets/js/props.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var btn = document.getElementById('btn')
+var anuncio = document.getElementById('anuncio-div')
+
+btn.addEventListener('click', function () {
+    anuncio.style.display = 'none'
+})
